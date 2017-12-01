715971727775bf32b4e55f3187b32194e5118142
david rugeles <rugeles.david@gmail.com>
2017-12-01 12:01:30 -0500

    se añade vista del login aunque aun no se integra, se crea el controlador para cuentas, se crean ramas para los cambios de cada uno¿ q

---
+++ senasistencia/Controllers/CuentasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace senasistencia.Controllers
+{
+    public class CuentasController : Controller
+    {
+        // GET: Cuenta
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
